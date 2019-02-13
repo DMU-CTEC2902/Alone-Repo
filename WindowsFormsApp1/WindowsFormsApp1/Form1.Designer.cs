daf94b91007ffce197b631f1a1c062fd166801bc
shefika <p17204160@my365.dmu.ac.uk>
2019-02-13 00:19:12 +0000

    Button added

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -31,43 +31,59 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.btnOk = new System.Windows.Forms.Button();
+            this.tst2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 126);
+            this.label1.Location = new System.Drawing.Point(174, 242);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.Size = new System.Drawing.Size(54, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Test";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 246);
+            this.button1.Location = new System.Drawing.Point(276, 473);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "test";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(138, 297);
+            this.btnOk.Location = new System.Drawing.Point(276, 571);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(150, 44);
             this.btnOk.TabIndex = 2;
             this.btnOk.Text = "Ok";
             this.btnOk.UseVisualStyleBackColor = true;
             // 
+            // tst2
+            // 
+            this.tst2.Location = new System.Drawing.Point(290, 670);
+            this.tst2.Name = "tst2";
+            this.tst2.Size = new System.Drawing.Size(136, 46);
+            this.tst2.TabIndex = 3;
+            this.tst2.Text = "test2";
+            this.tst2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.tst2);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,6 +96,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button tst2;
     }
 }
 
