e5531d496feea1877b3eb647fb93c6bff488d457
Connor19 <connor.reid98@btinternet.com>
2019-02-17 15:01:49 +0000

    Added Test project Folder With Colour Match game Inside (Test)

---
+++ Test Project/ColourMatch/ColourMatch/ColourHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ColourMatch
+{
+    public class ColourHelper
+    {
+
+        public static int Distance(Color firstColour, Color secondColour)
+        {
+         
+            int redDistance = Math.Abs(firstColour.R - secondColour.R);
+            int greenDistance = Math.Abs(firstColour.G - secondColour.G);
+            int blueDistance = Math.Abs(firstColour.B - secondColour.B);
+
+            int distance = (int)Math.Sqrt(redDistance * redDistance + greenDistance * greenDistance + blueDistance * blueDistance);
+
+            return distance;
+        }
+    }
+}
---
+++ Test Project/ColourMatch/ColourMatch/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColourMatch
+{
+    public partial class Form1 : Form
+    {
+        Game game;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBarRed_Scroll(object sender, EventArgs e)
+        {
+            game.GuessRed = trackBarRed.Value;
+            pictureBoxGuess.BackColor = game.Guess;
+            pictureBoxGuess.Invalidate();
+
+        }
+
+        private void trackBarGreen_Scroll(object sender, EventArgs e)
+        {
+            game.GuessGreen = trackBarGreen.Value;
+            pictureBoxGuess.BackColor = game.Guess;
+            pictureBoxGuess.Invalidate();
+
+        }
+
+        private void trackBarBlue_Scroll(object sender, EventArgs e)
+        {
+            game.GuessBlue = trackBarBlue.Value;
+            pictureBoxGuess.BackColor = game.Guess;
+            pictureBoxGuess.Invalidate();
+
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            int distance = ColourHelper.Distance(game.TargetColour, game.Guess);
+
+            // Show How Well The Player Does
+            MessageBox.Show(String.Format("Score: {0}", distance));
+
+            // Start New Game
+            StartNewGame();
+
+        }
+
+        public void StartNewGame()
+        {
+            game = new Game();
+            pictureBoxColour.BackColor = game.TargetColour;
+            //Reset Track Bars
+            trackBarRed.Value = 0;
+            trackBarGreen.Value = 0;
+            trackBarBlue.Value = 0;
+            pictureBoxGuess.BackColor = game.Guess;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
